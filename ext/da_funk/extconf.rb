eebd8596b799ff2aa5ba2c6b66972b8ab897b5ba
Thiago Scalone <thiago@scalone.com.br>
2014-12-05 15:38:54 -0200

    use bundle exec to compilation.

--- ext/da_funk/extconf.rb
+++ ext/da_funk/extconf.rb
@@ -3,6 +3,6 @@ require 'mkmf'
 
 rakefile = File.join(File.dirname(File.expand_path(__FILE__)), "..", "..", "Rakefile")
 
-sh "rake -f #{rakefile}"
+sh "bundle exec rake -f #{rakefile}"
 
 $makefile_created = true
