d9135c0d87194c7e3042b3b9a6cc6af96340c42f
Thiago Scalone <thiago@scalone.com.br>
2015-01-15 17:52:49 -0200

    Refactoring mruby bin and mrbc converting to a configuration method.

--- lib/da_funk/rake_task.rb
+++ lib/da_funk/rake_task.rb
@@ -9,7 +9,7 @@ module DaFunk
   class RakeTask < ::Rake::TaskLib
     include ::Rake::DSL if defined?(::Rake::DSL)
 
-    attr_accessor :name, :libs, :tests, :tests_unit, :tests_integration, :root_path, :main_out, :test_out, :resources
+    attr_accessor :name, :libs, :tests, :tests_unit, :tests_integration, :root_path, :main_out, :test_out, :resources, :mrbc, :mruby
 
     def initialize(name = :da_funk)
       @name = name
@@ -24,6 +24,8 @@ module DaFunk
       @root_path         ||= File.dirname("./")
       @main_out          ||= File.join(root_path, "out", "main.mrb")
       @test_out          ||= File.join(root_path, "out", "test.mrb")
+      @mruby             ||= "cloudwalk"
+      @mrbc              ||= "cloudwalk"
 
       define
     end
@@ -31,11 +33,7 @@ module DaFunk
     def execute_tests(files)
       all_files = FileList["test/test_helper.rb"] + files + ["test/test_run.rb"]
       if sh("mrbc -o #{main_out} #{libs.uniq}") && sh("mrbc -o #{test_out} #{all_files.uniq}")
-        if ENV["MRUBY_BIN"]
-          sh("#{ENV["MRUBY_BIN"]} out/test.mrb")
-        else
-          sh("mruby -b out/test.mrb")
-        end
+        sh("#{mruby} out/test.mrb")
       end
     end
 
@@ -62,11 +60,7 @@ module DaFunk
             sh "cp #{File.join(gem.full_gem_path, "out", gem.name)}.mrb out/#{gem.name}.mrb" if File.exists? "#{File.join(gem.full_gem_path, "out", gem.name)}.mrb"
           end
 
-          if ENV["MRBC"]
-            sh "#{ENV["MRBC"]} -o #{main_out} #{libs} "
-          else
-            sh "env mrbc -o #{main_out} #{libs}"
-          end
+          sh "#{mrbc} -o #{main_out} #{libs} "
         end
 
         namespace :mtest do
