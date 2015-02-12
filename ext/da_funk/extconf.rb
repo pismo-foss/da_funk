8afb74f1ee176f5fc1f48c42bce4083ee7ac1833
Thiago Scalone <thiago@scalone.com.br>
2015-02-12 17:53:25 -0200

    test bundle check.

--- ext/da_funk/extconf.rb
+++ ext/da_funk/extconf.rb
@@ -1,6 +1,8 @@
 p require 'rake'
 p require 'mkmf'
 
+p sh("bundle check")
+
 ENV["BUNDLE_GEMFILE"] = File.join(File.dirname(File.expand_path(__FILE__)), "..", "..", "Gemfile")
 p rakefile = File.join(File.dirname(File.expand_path(__FILE__)), "..", "..", "Rakefile")
 
