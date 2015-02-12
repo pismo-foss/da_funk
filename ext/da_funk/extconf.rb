d37e2d73f7dffec8f21e65cc25db795cd1e98686
Thiago Scalone <thiago@scalone.com.br>
2015-02-12 18:01:02 -0200

    Revert "Remove bundle from extconf."
    This reverts commit b00e5a588dbccb72ce734d84cd4f6059c6c279f0.

--- ext/da_funk/extconf.rb
+++ ext/da_funk/extconf.rb
@@ -1,8 +1,11 @@
 p require 'rake'
 p require 'mkmf'
 
+p sh("bundle check")
+
+ENV["BUNDLE_GEMFILE"] = File.join(File.dirname(File.expand_path(__FILE__)), "..", "..", "Gemfile")
 p rakefile = File.join(File.dirname(File.expand_path(__FILE__)), "..", "..", "Rakefile")
 
-if sh "rake -f #{rakefile}"
+if sh "bundle exec rake -f #{rakefile}"
   $makefile_created = true
 end
