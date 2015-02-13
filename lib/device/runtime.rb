d5cc43e3d7d33b7620f4ec864797a76466e8abdf
Thiago Scalone <thiago@scalone.com.br>
2015-02-13 13:43:46 -0200

    Refactoring Runtime execution.

--- lib/device/runtime.rb
+++ lib/device/runtime.rb
@@ -4,8 +4,19 @@ class Device
       Device.adapter::Runtime
     end
 
-    def self.execute(app)
-      self.adapter.execute(app)
+    # Execute app in new context.
+    #   To execute the should exists a zip file cotain the app,
+    #   previously downloaded from CloudWalk.
+    #
+    # @param app [String] App name, example "app", should exists file app.zip
+    # @param json [String] Parameters to confifure new aplication.
+    # @return [Object] From the new runtime instance.
+    def self.execute(app, json)
+      zip = "./#{app}.zip"
+      Device::Display.clear
+      raise Errno::ENOENT, zip unless File.exists?(zip)
+      raise "Problem to unzip #{zip}" unless Miniz.unzip(zip)
+      return mrb_eval "Context.start('#{app}', '#{Device.adapter}', '#{buf}')"
     end
   end
 end
