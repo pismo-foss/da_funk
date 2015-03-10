914870d4dd4db2329a2da0290fdccc68df625fb2
Thiago Scalone <thiago@scalone.com.br>
2015-03-10 16:55:00 -0300

    Add integration test mrb_eval_test

---
+++ test/integration/mrb_eval_test.rb
@@ -0,0 +1,36 @@
+
+class MrbEvalTest < DaFunk::Test.case
+  def test_socket
+    Device::Setting.environment = "staging"
+    Device::Setting.host = Device::Setting::HOST_STAGING
+    Device::Setting.company_name = "pc1"
+    Device::Setting.logical_number = "1234"
+
+    Device::Notification.start
+    Device::Notification.check
+
+    http = SimpleHttp.new('http', 'http://google.com', 443)
+    http.socket = Device::Network.walk_socket
+    assert_equal 302, http.get('/').code
+
+    command =<<EOF
+    require 'da_funk'
+    require 'da_funk/simplehttp'
+    require '../utils/command_line_platform.rb'
+    Device::Setting.environment = "staging"
+    Device::Setting.host = Device::Setting::HOST_STAGING
+    Device::Setting.company_name = "pc1"
+    Device::Setting.logical_number = "1234"
+
+    Device::Network.socket = nil
+    http = SimpleHttp.new('http', 'http://google.com', 80)
+    http.socket = Device::Network.walk_socket
+    response = http.get('/')
+    Device::Network.socket = nil
+    response.code
+EOF
+    command << " \n; mrb_eval(#{command.inspect})"
+
+    assert_equal 302, mrb_eval(command)
+  end
+end
