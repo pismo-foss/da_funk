9fda78d8346249e666a2b3549a5bbee7df2aeffe
Thiago Scalone <thiago@scalone.com.br>
2015-03-27 10:43:16 -0300

    comment sample to support flat method degradation between versions.

--- lib/device.rb
+++ lib/device.rb
@@ -8,9 +8,10 @@ class Device
   # @return [Class] the class object flatted
   def self.flat_api
     klass_version = Device.version.gsub(".", "")
-    if Device.version == "0.4.3"
-      const_get("VersionFlat#{klass_version}").flat Device
-    end
+    # Sample
+    #if Device.version == "0.4.3"
+      #const_get("VersionFlat#{klass_version}").flat Device
+    #end
   end
 
   def self.call(*args)
