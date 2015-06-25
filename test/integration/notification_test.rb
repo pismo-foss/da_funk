c324415fc70c90709b4fcadc2dbe9f091feeeb7a
Thiago Scalone <thiago@scalone.com.br>
2015-06-25 20:31:20 -0300

    Add integration test to notication keep alive.

--- test/integration/notification_test.rb
+++ test/integration/notification_test.rb
@@ -16,5 +16,21 @@ class NotificationTest < DaFunk::Test.case
     notification.close
     assert notification.closed?
   end
+
+  def test_notification_50_checks
+    Device::Setting.to_staging!
+    Device::Setting.company_name = "pc1"
+    Device::Setting.logical_number = "0101"
+    Device::Notification.setup
+    notification = Device::Notification.new
+
+    50.times do
+      notification.check
+      sleep 2
+    end
+
+    notification.close
+    assert notification.closed?
+  end
 end
 
