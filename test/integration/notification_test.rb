15739dea2de06145dc145d8a421a3bb7e6a09ae3
Thiago Scalone <thiago@scalone.com.br>
2015-07-17 20:37:23 -0300

    Refactoring Notifications tests to check SHOW_MESSAGE.

--- test/integration/notification_test.rb
+++ test/integration/notification_test.rb
@@ -6,6 +6,7 @@ class NotificationTest < DaFunk::Test.case
     Device::Setting.logical_number = "0101"
     Device::Notification.setup
     notification = Device::Notification.new
+
     assert_equal nil, notification.check
     notification.check
     notification.check
@@ -19,17 +20,25 @@ class NotificationTest < DaFunk::Test.case
     assert notification.closed?
   end
 
-  def test_notification_50_checks
+  def test_notification_SHOW_MESSAGE
+    $status = nil
     Device::Setting.to_staging!
     Device::Setting.company_name = "pc1"
     Device::Setting.logical_number = "0101"
     Device::Notification.setup
+    NotificationCallback.new "SHOW_MESSAGE", :on => Proc.new { |message,datetime|
+      $status = message
+    }
+
     notification = Device::Notification.new
 
-    50.times do
+    puts "Must create message AAAA"
+    time = Time.now + 60
+    while(time > Time.now && $status.nil?) do
       notification.check
       sleep 2
     end
+    assert_equal "AAAA", $status
 
     notification.close
     assert notification.closed?
