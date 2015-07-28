e03b789de311c49f216091e100f0e567e380ee62
Thiago Scalone <thiago@scalone.com.br>
2015-07-28 18:12:45 -0300

    Fix get_string loop stack deep, calling gets from STDIN.

--- utils/command_line_platform.rb
+++ utils/command_line_platform.rb
@@ -11,7 +11,7 @@ class CommandLinePlatform
     def self.get_string(min, max, option = nil)
       str = ""
       while
-        str << gets.chomp
+        str << STDIN.gets.chomp
         return str if str.size >= min
       end
     end
