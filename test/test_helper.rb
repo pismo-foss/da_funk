514cc80f8ef3ee78b5109d54c575d30e7c66fe3b
Thiago Scalone <thiago@scalone.com.br>
2015-08-03 21:27:00 -0300

    Refactoring test_helper to remove CRUBY support.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,18 +1,11 @@
+ROOT_PATH = File.expand_path("./")
+APP_NAME = File.basename(File.dirname(ROOT_PATH))
 
-if Object.const_defined?(:MTest)
-  ROOT_PATH = File.expand_path("./")
-  APP_NAME = File.basename(File.dirname(ROOT_PATH))
+$LOAD_PATH.unshift "./#{APP_NAME}"
 
-  $LOAD_PATH.unshift "./#{APP_NAME}"
+require 'da_funk'
 
-  require 'da_funk'
-
-  DaFunk::Test.configure do |t|
-    t.root_path = ROOT_PATH
-  end
-else
-  require "./lib/da_funk.rb"
-  require "test/unit"
-  require "./test/fixtures/platform.rb"
+DaFunk::Test.configure do |t|
+  t.root_path = ROOT_PATH
 end
 
