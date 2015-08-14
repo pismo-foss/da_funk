b967342205e589a23eea214afefb542eebafd85c
Thiago Scalone <thiago@scalone.com.br>
2015-08-14 17:38:39 -0300

    Remove native extensions.

--- ext/da_funk/extconf.rb
+++
@@ -1,8 +0,0 @@
-require 'rake'
-require 'mkmf'
-
-rakefile = File.join(File.dirname(File.expand_path(__FILE__)), "..", "..", "Rakefile")
-
-if sh "bundle exec rake -f #{rakefile}"
-  $makefile_created = true
-end
