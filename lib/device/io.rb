8065bca6c33044199a82013b1ad2408b6dfa79c6
Thiago Scalone <thiago@scalone.com.br>
2015-11-09 12:24:31 -0200

    Refactoring Device::IO.get_format: - Add support masquerade values. - Improve the input letters and numbers. - Add input alpha.

--- lib/device/io.rb
+++ lib/device/io.rb
@@ -17,27 +17,58 @@ class Device
     CANCEL           = 0x1B.chr
     IO_INPUT_NUMBERS = :numbers
     IO_INPUT_LETTERS = :letters
+    IO_INPUT_ALPHA   = :alpha
     IO_INPUT_SECRET  = :secret
     IO_INPUT_DECIMAL = :decimal
     IO_INPUT_MONEY   = :money
     IO_INPUT_MASK    = :mask
 
+    MASK_ALPHA       = :alpha
+    MASK_LETTERS     = :letters
+    MASK_NUMBERS     = :number
+
     DEFAULT_TIMEOUT  = 30000
 
     NUMBERS = %w(1 2 3 4 5 6 7 8 9 0)
 
-    ONE   = "1qzQZ _,."
-    TWO   = "2abcABC"
-    THREE = "3defDEF"
-    FOUR  = "4ghiGHI"
-    FIVE  = "5jklJKL"
-    SIX   = "6mnoMNO"
-    SEVEN = "7prsPRS"
-    EIGHT = "8tuvTUV"
-    NINE  = "9wxyWXY"
-    ZERO  = "0spSP"
+    ONE_LETTERS   = "qzQZ _,."
+    TWO_LETTERS   = "abcABC"
+    THREE_LETTERS = "defDEF"
+    FOUR_LETTERS  = "ghiGHI"
+    FIVE_LETTERS  = "jklJKL"
+    SIX_LETTERS   = "mnoMNO"
+    SEVEN_LETTERS = "prsPRS"
+    EIGHT_LETTERS = "tuvTUV"
+    NINE_LETTERS  = "wxyWXY"
+    ZERO_LETTERS  = "spSP"
 
-    KEYS_RANGE = [ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, ZERO]
+    ONE_NUMBER    = "1"
+    TWO_NUMBER    = "2"
+    THREE_NUMBER  = "3"
+    FOUR_NUMBER   = "4"
+    FIVE_NUMBER   = "5"
+    SIX_NUMBER    = "6"
+    SEVEN_NUMBER  = "7"
+    EIGHT_NUMBER  = "8"
+    NINE_NUMBER   = "9"
+    ZERO_NUMBER   = "0"
+
+    ONE_ALPHA     = ONE_NUMBER   + ONE_LETTERS
+    TWO_ALPHA     = TWO_NUMBER   + TWO_LETTERS
+    THREE_ALPHA   = THREE_NUMBER + THREE_LETTERS
+    FOUR_ALPHA    = FOUR_NUMBER  + FOUR_LETTERS
+    FIVE_ALPHA    = FIVE_NUMBER  + FIVE_LETTERS
+    SIX_ALPHA     = SIX_NUMBER   + SIX_LETTERS
+    SEVEN_ALPHA   = SEVEN_NUMBER + SEVEN_LETTERS
+    EIGHT_ALPHA   = EIGHT_NUMBER + EIGHT_LETTERS
+    NINE_ALPHA    = NINE_NUMBER  + NINE_LETTERS
+    ZERO_ALPHA    = ZERO_NUMBER  + ZERO_LETTERS
+
+    RANGE_ALPHA   = [ONE_ALPHA, TWO_ALPHA, THREE_ALPHA, FOUR_ALPHA, FIVE_ALPHA, SIX_ALPHA, SEVEN_ALPHA, EIGHT_ALPHA, NINE_ALPHA, ZERO_ALPHA]
+    RANGE_NUMBER  = [ONE_NUMBER, TWO_NUMBER, THREE_NUMBER, FOUR_NUMBER, FIVE_NUMBER, SIX_NUMBER, SEVEN_NUMBER, EIGHT_NUMBER, NINE_NUMBER, ZERO_NUMBER]
+    RANGE_LETTERS = [ONE_LETTERS, TWO_LETTERS, THREE_LETTERS, FOUR_LETTERS, FIVE_LETTERS, SIX_LETTERS, SEVEN_LETTERS, EIGHT_LETTERS, NINE_LETTERS, ZERO_LETTERS]
+
+    KEYS_RANGE    = {MASK_ALPHA => RANGE_ALPHA, MASK_LETTERS => RANGE_LETTERS, MASK_NUMBERS => RANGE_NUMBER}
 
     include Device::Helper
 
@@ -62,21 +93,23 @@ class Device
     #
     # :label - Sets the label display before currency, eg.: "U$:", "R$:"
     #
+    # :mask - If mode IO_INPUT_MASK a mask should send
+    #   (only numbers and letters are allowed), eg.: "9999-AAAA"
+    #
     # :mode - Define input modes:
     #
     #   :numbers (IO_INPUT_NUMBERS) - Only number.
-    #   :letters (IO_INPUT_LETTERS) - Letters and numbers.
+    #   :letters (IO_INPUT_LETTERS) - Only Letters.
+    #   :alpha (IO_INPUT_ALPHA) - Letters and numbers.
     #   :secret (IO_INPUT_SECRET) - Secret *.
     #   :decimal (IO_INPUT_DECIMAL) - Decimal input, only number.
     #   :money (IO_INPUT_MONEY) - Money input, only number.
+    #   :mask (IO_INPUT_MASK) - Custom mask.
     #
     # @return [String] buffer read from keyboard
     def self.get_format(min, max, options = {})
-      options[:mode]   ||= IO_INPUT_LETTERS
-      options[:line]   ||= 2
-      options[:column] ||= 0
-      text = ""
-      key = ""
+      set_default_format_option(options)
+      key = text = ""
 
       while key != CANCEL
         Device::Display.clear options[:line]
@@ -87,7 +120,7 @@ class Device
         elsif key == ENTER
           return text
         elsif key ==  F1 || key == DOWN || key == UP
-          change_next(text) if (options[:mode] != IO_INPUT_MONEY) && (options[:mode] != IO_INPUT_DECIMAL)
+          change_next(text, check_mask_type(text, options))
           next
         elsif text.size >= max
           next
@@ -97,9 +130,29 @@ class Device
       end
     end
 
-    def self.change_next(text)
+    def self.set_default_format_option(options)
+      options[:mode]   ||= IO_INPUT_LETTERS
+      options[:line]   ||= 2
+      options[:column] ||= 0
+
+      if options[:mask]
+        options[:mask_clean] = options[:mask].chars.reject{|ch| ch.match(/[^0-9A-Za-z]/) }.join
+      end
+    end
+
+    def self.check_mask_type(text, options)
+      if options[:mode] == Device::IO::IO_INPUT_ALPHA
+        Device::IO::MASK_ALPHA
+      elsif options[:mode] == Device::IO::IO_INPUT_LETTERS
+        Device::IO::MASK_LETTERS
+      elsif options[:mode] == Device::IO::IO_INPUT_MASK
+        options[:mask_clean][text.length - 1].match(/[0-9]/) ? Device::IO::MASK_NUMBERS : Device::IO::MASK_LETTERS
+      end
+    end
+
+    def self.change_next(text, mask_type = Device::IO::MASK_ALPHA)
       char = text[-1]
-      if range = KEYS_RANGE.detect { |range| range.include?(char) }
+      if range = KEYS_RANGE[mask_type].detect { |range| range.include?(char) }
         index = range.index(char)
         new_value = range[index+1]
         if new_value
--- test/unit/device/io_test.rb
+++ test/unit/device/io_test.rb
@@ -1,6 +1,6 @@
 
 class IOTest < DaFunk::Test.case
-  def test_io_change_next
+  def test_io_change_next_alphanumeric_mask
     text = "TES8"
     assert_equal "TESt", Device::IO.change_next(text)
     assert_equal "TESu", Device::IO.change_next(text)
@@ -9,6 +9,22 @@ class IOTest < DaFunk::Test.case
     assert_equal "TESU", Device::IO.change_next(text)
     assert_equal "TESV", Device::IO.change_next(text)
     assert_equal "TES8", Device::IO.change_next(text)
+  end
+
+  def test_io_change_next_number_mask
+    text = "TES8"
+    assert_equal "TES8", Device::IO.change_next(text, :number)
+    assert_equal "TES8", Device::IO.change_next(text, :number)
+  end
+
+  def test_io_change_next_letters_mask
+    text = "TESt"
+    assert_equal "TESu", Device::IO.change_next(text, :letters)
+    assert_equal "TESv", Device::IO.change_next(text, :letters)
+    assert_equal "TEST", Device::IO.change_next(text, :letters)
+    assert_equal "TESU", Device::IO.change_next(text, :letters)
+    assert_equal "TESV", Device::IO.change_next(text, :letters)
+    assert_equal "TESt", Device::IO.change_next(text, :letters)
   end
 
   def test_io_insert_key_number
@@ -48,6 +64,71 @@ class IOTest < DaFunk::Test.case
   def test_io_format_string_to_secret
     options = {:mode => Device::IO::IO_INPUT_SECRET}
     assert_equal "*******", Device::IO.format("1000000", options)
+  end
+
+  def test_io_set_default_format_option
+    options = {}
+    options_default = {:mode => Device::IO::IO_INPUT_LETTERS, :line => 2, :column => 0}
+    Device::IO.set_default_format_option(options)
+    assert_equal options_default, options
+  end
+
+  def test_io_set_default_format_option_for_mask
+    options = {:mode => Device::IO::IO_INPUT_MASK, :mask => "999-AAA-999/99.99"}
+    options_default = {
+      :mode => Device::IO::IO_INPUT_MASK,
+      :mask_clean => "999AAA9999999",
+      :mask => "999-AAA-999/99.99",
+      :line => 2, :column => 0
+    }
+    Device::IO.set_default_format_option(options)
+    assert_equal options_default, options
+  end
+
+  def test_io_check_mask_type_alpha
+    options = { :mode => Device::IO::IO_INPUT_ALPHA }
+    assert_equal Device::IO::MASK_ALPHA, Device::IO.check_mask_type("", options)
+  end
+
+  def test_io_check_mask_type_number
+    options = { :mode => Device::IO::IO_INPUT_LETTERS }
+    assert_equal Device::IO::MASK_LETTERS, Device::IO.check_mask_type("", options)
+  end
+
+  def test_io_check_mask_type_number_mask
+    options = {
+      :mode => Device::IO::IO_INPUT_MASK,
+      :mask => "999-999/99.99",
+      :mask_clean => "9999999999"
+    }
+    assert_equal Device::IO::MASK_NUMBERS, Device::IO.check_mask_type("", options)
+  end
+
+  def test_io_check_mask_type_letters_mask
+    options = {
+      :mode => Device::IO::IO_INPUT_MASK,
+      :mask => "AAA-AAA/AA.AA",
+      :mask_clean => "AAAAAAAAAA"
+    }
+    assert_equal Device::IO::MASK_LETTERS, Device::IO.check_mask_type("", options)
+  end
+
+  def test_io_check_mask_type_mix_letters_mask
+    options = {
+      :mode => Device::IO::IO_INPUT_MASK,
+      :mask => "999-A99/99.99",
+      :mask_clean => "999A999999"
+    }
+    assert_equal Device::IO::MASK_LETTERS, Device::IO.check_mask_type("111B", options)
+  end
+
+  def test_io_check_mask_type_mix_number_mask
+    options = {
+      :mode => Device::IO::IO_INPUT_MASK,
+      :mask => "AAA-9AA/AA.AA",
+      :mask_clean => "AAA9AAAAAA"
+    }
+    assert_equal Device::IO::MASK_NUMBERS, Device::IO.check_mask_type("SDF4", options)
   end
 end
 
