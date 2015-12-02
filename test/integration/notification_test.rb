cc0f02e8b1f45ad73804e606c1566474ebf189d0
Thiago Scalone <thiago@scalone.com.br>
2015-12-02 11:34:55 -0200

    Refactor NotificationTest.

--- test/integration/notification_test.rb
+++ test/integration/notification_test.rb
@@ -1,9 +1,7 @@
 
 class NotificationTest < DaFunk::Test.case
   def test_notification
-    Device::Setting.to_staging!
     Device::Setting.company_name = "pc1"
-    Device::Setting.logical_number = "0101"
     Device::Notification.setup
     notification = Device::Notification.new
 
@@ -22,17 +20,16 @@ class NotificationTest < DaFunk::Test.case
 
   def test_notification_SHOW_MESSAGE
     $status = nil
-    Device::Setting.to_staging!
     Device::Setting.company_name = "pc1"
-    Device::Setting.logical_number = "0101"
     Device::Notification.setup
-    NotificationCallback.new "SHOW_MESSAGE", :on => Proc.new { |message,datetime|
+    Device::NotificationCallback.new "SHOW_MESSAGE", :on => Proc.new { |message,datetime|
       $status = message
     }
 
     notification = Device::Notification.new
 
-    puts "Must create message AAAA"
+    p "Must create message AAAA"
+    p "Remember to Press Enter after message appears"
     time = Time.now + 60
     while(time > Time.now && $status.nil?) do
       notification.check
