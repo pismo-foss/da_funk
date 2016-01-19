3ac510a5748170342de6be7bf5e2a5df0d318e74
Thiago Scalone <thiago@scalone.com.br>
2016-01-19 11:59:59 -0300

    Refactoring Application class: - Improved outdated method to check crc file only when is necessary, thinking on performance. - Add support to return FILE_NOT_CHANGE with the crc is the same without get in the host. - Implemented Application#delete.

--- lib/device/application.rb
+++ lib/device/application.rb
@@ -1,33 +1,61 @@
+# Scenario
+# 1. Load from scratch
+# - No file downloaded
+# - with file downloaded
+# 2. Second load
+# - No crc update
+# - With crc update
+
 class Device
   class Application
-    attr_reader :label, :file_path, :type, :crc, :order, :name
+    attr_reader :label, :file, :type, :crc, :order, :name, :remote, :original,
+      :crc_local
 
-    def initialize(label, file_path, type, crc)
-      @name = label
+    def self.delete(collection)
+      collection.each do |app|
+        app.delete
+      end
+    end
+
+    def initialize(label, remote, type, crc)
+      @type     = type
+      @crc      = crc
+      @original = remote
       @order, @label = split_label(label)
-      @file_path = file_path
-      @type      = type
-      @crc       = crc
+      @remote    = remote.sub("#{Device::Setting.company_name}_", "")
+      @name      = remote.sub("#{Device::Setting.company_name}_", "").split(".")[0]
+      @file      = check_path(@remote)
+      @crc_local = @crc if File.exists?(@file)
     end
 
-    def file
-      @file ||= file_path.gsub("#{Device::Setting.company_name}_", "")
+    def download(force = false)
+      if force || outdated?
+        ret = Device::Transaction::Download.request_file(remote, file, crc_local)
+      else
+        ret = Device::Transaction::Download::FILE_NOT_CHANGE
+      end
+      @crc_local = @crc if ret == Device::Transaction::Download::SUCCESS
+      ret
     end
 
-    def file_no_ext
-      @file_no_ext ||= file.split(".")[0]
+    def dir
+      @name
     end
 
-    def zip
-      @zip ||= "#{file_no_ext}.zip"
+    def delete
+      File.delete(self.file) if File.exists?(self.file)
+      Dir.delete(self.dir) if Dir.exist?(self.dir)
     end
 
     def outdated?
-      return false unless File.exists?(@file_path)
-      file = File.open(@file_path)
-      Device::Crypto.crc16_hex(file.read) == @crc
+      return true unless File.exists?(file)
+      unless @crc_local
+        handle = File.open(file)
+        @crc_local = Device::Crypto.crc16_hex(handle.read)
+      end
+      @crc_local != @crc
     ensure
-      file.close if file
+      handle.close if handle
     end
 
     def execute(json = "")
@@ -47,6 +75,15 @@ class Device
     end
 
     private
+
+    def check_path(path)
+      if posxml?
+        "./shared/#{path}"
+      else
+        "#{path.gsub("#{Device::Setting.company_name}_", "")}.zip"
+      end
+    end
+
     def split_label(text)
       if text == "X"
         number, text = 0, "X"
