e16b428505b39a748c1ad3e3f8722a19ba02d270
Thiago Scalone <thiago@scalone.com.br>
2016-02-02 15:31:11 -0200

    Fix ISO8583::FileParser: - Close file if file was open. - Fix FileParse.parse_file and FileParse.create_mki call.

--- lib/iso8583/file_parser.rb
+++ lib/iso8583/file_parser.rb
@@ -11,16 +11,16 @@ module ISO8583
       end
     end
 
-    def create_mti(klass, mti_format, mti_messages)
-      klass.mti_format *mti_format
+    def self.create_mti(klass, format, mti_messages)
+      klass.mti_format(*format)
       mti_messages.each do |number, description|
         klass.mti number, description
       end
     end
 
-    def parse_file(filepath)
-      return [] unless File.exists?(path)
-      file = File.open(path, "r")
+    def self.parse_file(filepath)
+      return [] unless File.exists?(filepath)
+      file = File.open(filepath, "r")
 
       file.readlines.inject([]) do |array, line|
         data = line.match(/([0-9]+)=\"ATT_([a-zA-Z]+);([0-9A-Z]+)/)
@@ -28,17 +28,15 @@ module ISO8583
         array << mount_parameters(data)
       end
     ensure
-      file.close
+      file.close if file
     end
 
     def self.build_klass(mti_format, mti_messages, filepath = PATH_DEFAULT)
       Class.new(ISO8583::Message) do |klass|
         klass.include ISO8583
-        #klass.mti_format ISO8583::N, :length => 4
-        #klass.mti 200, "Authorization Request Acquirer Gateway"
-        create_mti(klass, mti_format, mti_messages)
+        FileParser.create_mti(klass, mti_format, mti_messages)
 
-        parse_file(filepath).each do |parameters|
+        FileParser.parse_file(filepath).each do |parameters|
           klass.bmp(*parameters)
         end
       end
