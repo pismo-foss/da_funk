980bf4d3317835577b7bb0b2aaabc3649990f4b7
Thiago Scalone <thiago@scalone.com.br>
2016-02-16 12:16:40 -0200

    Refactoring application_test.

--- test/unit/device/application_test.rb
+++ test/unit/device/application_test.rb
@@ -1,9 +1,9 @@
 
 class ApplicationTest < DaFunk::Test.case
   def setup
-    @file_path = "./ttt"
+    @file_path = "#{Device::Setting.company_name}_ttt"
     @string = "12345678901234567890"
-    @file = File.open("./ttt", "w+")
+    @file = File.open("./ttt.zip", "w+")
     @file.write(@string)
     @file.close
     @crc = Device::Crypto.crc16_hex(@string)
@@ -11,21 +11,21 @@ class ApplicationTest < DaFunk::Test.case
   end
 
   def test_check_crc_true
-    application = Device::Application.new("TTT", @file_path, "ruby", @crc)
+    application = Device::Application.new("TTT", @file_path, "ruby", nil)
     assert_equal true, application.outdated?
   end
 
   def test_check_crc_false
-    application = Device::Application.new("TTT", @file_path, "ruby", "1111")
+    application = Device::Application.new("TTT", @file_path, "ruby", @crc)
     assert_equal false, application.outdated?
   end
 
   def test_check_crc_non_file
     application = Device::Application.new("TTT", "./non_exists", "ruby", "1111")
-    assert_equal false, application.outdated?
+    assert_equal true, application.outdated?
   end
 
   def teardown
-    File.delete(@file_path)
+    File.delete("./ttt.zip")
   end
 end
