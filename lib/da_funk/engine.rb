f5e7e5d4860a4757b3d5f57afe5f41e8cc8d9e8a
Thiago Scalone <thiago@scalone.com.br>
2016-03-14 12:14:37 -0300

    Remove Notification setup from app_loop and DaFunk responsibility.

--- lib/da_funk/engine.rb
+++ lib/da_funk/engine.rb
@@ -1,19 +1,13 @@
 module DaFunk
   class Engine
-    def self.setup
-      Device::Display.clear
-      puts "SETUP NOTIFICATIONS..."
-      Device::Notification.setup
-    end
-
     def self.check
       DaFunk::EventListener.check
       Device::Notification.check
       DaFunk::Helper::StatusBar.check
     end
+
     def self.app_loop(&block)
       @stop = false
-      self.setup
       loop do
         self.check
         break if @stop
