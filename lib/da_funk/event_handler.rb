3ffd2a72d96a615ea239c1ea3b8c946a382a7cc5
Thiago Scalone <thiago@scalone.com.br>
2016-07-06 10:24:17 -0300

    Add support for timer check on EventHandler.

--- lib/da_funk/event_handler.rb
+++ lib/da_funk/event_handler.rb
@@ -10,11 +10,23 @@ module DaFunk
     end
 
     def register
+      self.schedule_timer
       EventListener.add_handler(self)
     end
 
     def perform(*parameter)
+      self.schedule_timer
       @perform_block.call(*parameter)
+    end
+
+    def schedule_timer
+      if self.option.is_a?(Hash) && self.option.include?(:minutes)
+        self.timer = Time.now + (self.option[:minutes].to_i * 60)
+      end
+    end
+
+    def execute?
+      self.timer < Time.now if self.timer
     end
   end
 end
